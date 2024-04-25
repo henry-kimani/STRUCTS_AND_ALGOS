b12d780875bab6346558fe75544259b5f9ae1ecf
kim henry <kimanihenry14489@gmail.com>
2024-04-25 10:32:49 +0300

    Added SinglyLinkedList.java and added the ListNode class that intializes the data for the nodes to be created

---
+++ java/SinglyLinkedList.java
@@ -0,0 +1,28 @@
+public class SinglyLinkedList {
+
+    // the head for the list that will be created 
+    private ListNode head;
+
+    // Creates the list nodes
+    private static class ListNode {
+        // instance variables
+
+        // the variable that holds the data in the node
+        private int data; // should be a generic type
+
+        // the variable that help point to the next node
+        private ListNode next;
+
+        // When creating an instance of the class, it will require the data to store
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    public static void main(String args[]){
+        // creating a list
+
+        
+    }
+}
